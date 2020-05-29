33fa292e3481da1d9bdea3b542d09e909f108630
Strukov_Bohdan <bodya19981997@gmail.com>
2020-05-29 18:54:15 +0300

    add CSS animation + refactoring on classList

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function hideTabContent() {
         tabsContent.forEach(item => {
-            item.style.display = 'none';
+            // item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
 
         tabs.forEach(item => {
@@ -15,7 +17,10 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function showTabContent(i = 0) {
-        tabsContent[i].style.display = 'block';
+        // tabsContent[i].style.display = 'block';
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+
         tabs[i].classList.add('tabheader__item_active');
     }
 
