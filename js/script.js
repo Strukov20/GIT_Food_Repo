bf687c4ee4ea597d21260abb30b0c0ea6f06c723
Strukov_Bohdan <bodya19981997@gmail.com>
2020-06-01 16:04:04 +0300

    update timer + refactoring modal window

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // TIMER
 
-    const deadLine = '2020-06-01';
+    const deadLine = '2020-06-10';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -89,9 +89,67 @@ window.addEventListener('DOMContentLoaded', () => {
 
         if(t.total <= 0) {
             clearInterval(timeInterval);
+            days.textContent = '00';
+            hours.textContent = '00';
+            minutes.textContent = '00';
+            seconds.textContent = '00';
         }
     }
 
     }
     setClock('.timer', deadLine);
+
+    //MODAL Window
+
+    function modal () {
+
+        const btnMore = document.querySelectorAll('[data-modal]'),
+            modal = document.querySelector('.modal'),
+            modalClose = document.querySelector('[data-close]');
+
+            function openModal() {
+                modal.classList.add('show');
+                modal.classList.remove('hide');
+                document.body.style.overflow = 'hidden';
+
+                clearInterval(modalTimerId); // Якщо користувач уже выдкрив модальне вікно то не відкривати його знову.
+            }
+
+            btnMore.forEach(btn => {
+                btn.addEventListener('click', openModal);
+            });
+
+
+            function closeModal() {
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+           } 
+
+            modalClose.addEventListener('click', closeModal);
+
+            modal.addEventListener('click', (e) => {
+                if (e.target === modal){
+                    closeModal();
+                }
+            });
+
+            document.addEventListener('keydown', (e) => {
+                if(e.code === "Escape" && modal.classList.contains('show')) {
+                    closeModal();
+                }
+            });
+
+            const modalTimerId = setTimeout(openModal, 5000);
+
+            function showModalByScroll() {
+                    if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+                    openModal();
+                    window.removeEventListener('scroll', showModalByScroll);
+                }
+            }
+            window.addEventListener('scroll', showModalByScroll);
+
+    }
+    modal();
 });
