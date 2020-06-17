e37773a5a7378a5bede635337bbce71214488fb9
Bohdan <bodyanu4killer1@gmail.com>
2020-06-17 10:44:41 +0300

    add json server

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -324,6 +324,10 @@ function forms () {
             closeModal();
         }, 4000);
     }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
 }
 
 module.exports = forms;
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -121,6 +121,10 @@ function forms () {
             closeModal();
         }, 4000);
     }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
 }
 
 module.exports = forms;
